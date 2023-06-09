28bfb957cf5edd0dfd7d79bdb693fdaae0c92beb
digidoor <37601466+digidoor@users.noreply.github.com>
2023-06-09 05:43:00 -0700

    added columns to models per requirements spec

--- models/Product.js
+++ models/Product.js
@@ -8,16 +8,49 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
-  },
-  {
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'product',
-  }
+{
+	// define columns
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		primaryKey: true,
+		autoIncrement: true,
+	},
+	product_name: {
+		type: DataTypes.STRING,
+		allowNull: false,
+	},
+	price: {
+		type: DataTypes.INTEGER, //should be in cents to be censible, hyuk hyuk hyuk
+		allowNull: false,
+		validate: {
+			isNumeric: true,
+		},
+	},
+	stock: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		defaultVaule: 10,
+		validate: {
+			isNumeric: true,
+		},
+	},
+	category_id: {
+		type: DataTypes.INTEGER,
+		allowNull: true,
+		references: {
+			model: 'category',
+			key: 'id',
+		},
+	},
+},
+{
+	sequelize,
+	timestamps: false,
+	freezeTableName: true,
+	underscored: true,
+	modelName: 'product',
+}
 );
 
 module.exports = Product;
